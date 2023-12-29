890d85d3db9700b091075a22f047d6d7c1f3e9fb
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2023-12-30 01:49:03 +0700

    abc

---
+++ ETL/DELETE DDS.sql
@@ -0,0 +1,9 @@
+USE SuperSale_DDS;
+
+DELETE FROM Fact_Sales;
+DELETE FROM Dim_City;
+DELETE FROM Dim_CustomerType;
+DELETE FROM Dim_Date;
+DELETE FROM Dim_Gender;
+DELETE FROM Dim_PaymentMethod;
+DELETE FROM Dim_ProductLine;
